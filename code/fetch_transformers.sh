d8d7e0e436427c86087117101d8bc7ec1d1d3842
philippeitis <33013301+philippeitis@users.noreply.github.com>
2021-09-08 14:41:46 -0700

    Check if pip install of kenlm is successful before cloning

--- code/fetch_transformers.sh
+++ code/fetch_transformers.sh
@@ -1,7 +1,11 @@
 git clone https://github.com/jeniyat/Attentive_Transformer_NER.git
 cd Attentive_Transformer_NER && pip install . && cd ..
-git clone https://github.com/kpu/kenlm/
-cd kenlm/python
-cython -3 --cplus kenlm.pyx
-cd ..
-pip install .
+
+if ! pip install kenlm ; then
+    git clone https://github.com/kpu/kenlm/
+    cd kenlm/python
+    cython -3 --cplus kenlm.pyx
+    cd ..
+    pip install .
+    cd ..
+fi
